677b26a3700376b3f37880d0974e85f61224ce12
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-24 15:07:51 -0300

    feat: add loginRequest on auth reducer

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -3,7 +3,7 @@ import * as types from '../types';
 const initialState = {
   isLoggedIn: false,
   token: false,
-  user: {},
+  usuario: {},
   isLoading: false,
 };
 
@@ -14,12 +14,19 @@ export default function (state = initialState, action) {
       const newState = { ...state };
       newState.isLoggedIn = true;
       newState.token = action.payload.token;
-      newState.user = action.payload.user;
+      newState.usuario = action.payload.usuario;
+      newState.isLoading = false;
       return newState;
     }
 
     case types.LOGIN_FAILURE: {
       const newState = { ...initialState };
+      return newState;
+    }
+
+    case types.LOGIN_REQUEST: {
+      const newState = { ...initialState };
+      newState.isLoading = true;
       return newState;
     }
 
