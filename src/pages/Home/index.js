dea8c6efe8be984f2c0305f14e041e97caa8766f
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-24 15:05:56 -0300

    feat: add Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <button type="button">Botão</button>
+    </>
+  );
+}
