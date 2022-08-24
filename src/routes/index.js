6d7a955c616fb21c57ec2b19ffc3c03c4a2a238c
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-24 15:07:11 -0300

    feat: add Home on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
+import Home from '../pages/Home';
 import Video from '../pages/Video';
 import Curso from '../pages/Curso';
 import Treinamento from '../pages/Treinamento';
@@ -11,7 +12,7 @@ import Error from '../pages/Error';
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} />
+      <MyRoute exact path="/" component={Home} />
       <MyRoute exact path="/login" component={Login} />
       <MyRoute exact path="/treinamentos" component={Treinamento} isClosed />
       <MyRoute exact path="/curso" component={Curso} isClosed />
