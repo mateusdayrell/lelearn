72902e0abafa0150acf93c29d6067d5cc2f315d5
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-24 13:35:15 -0300

    feat: add form on login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,61 @@
-import React from 'react';
-// import { useDispatch } from 'react-redux/es/exports';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux/es/exports';
+import { get } from 'lodash';
 
-// import * as logadoActions from '../../store/modules/logado/actions';
+import * as actions from '../../store/modules/auth/actions';
 import './style.css';
 
-export default function Login() {
-  // const dispatch = useDispatch();
+export default function Login(props) {
+  const dispatch = useDispatch();
 
-  // const handleClick = (e) => {
-  //   e.preventDefault();
+  const prevPath = get(props, 'location.state.prevPath', '/');
 
-  //   dispatch(logadoActions.logadoRequest());
-  // };
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErros = false;
+
+    if (!email) {
+      formErros = true;
+      toast.info('Digite um e-mail');
+    }
+
+    if (!senha) {
+      formErros = true;
+      toast.info('Digite uma senha');
+    }
+
+    if (formErros) return;
+
+    dispatch(actions.loginRequest({ email, senha, prevPath }));
+  };
 
   return (
     <>
       <h1>Login</h1>
-      <button type="button">Logar</button>
+
+      <div className="container">
+        <form onSubmit={handleSubmit} className="form">
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="senha"
+            placeholder="senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          <button type="submit">Logar</button>
+        </form>
+      </div>
     </>
   );
 }
