95cea34af990c6a46d16e8bbd38a35d4b13b4ec4
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-29 14:33:18 -0300

    feat: add usuario on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
@@ -8,14 +9,17 @@ import Video from '../pages/Video';
 import Curso from '../pages/Curso';
 import Treinamento from '../pages/Treinamento';
 import Error from '../pages/Error';
+import AdmUsuario from '../pages/Admin/Usuario';
 
 export default function Routes() {
+  // const tipoUsuario = useSelector((state) => state.auth.usuario.tipo);
   return (
     <Switch>
       <MyRoute exact path="/" component={Home} />
       <MyRoute exact path="/login" component={Login} />
       <MyRoute exact path="/treinamentos" component={Treinamento} isClosed />
       <MyRoute exact path="/curso" component={Curso} isClosed />
+      <MyRoute exact path="/usuarios" component={AdmUsuario} isClosed />
       <MyRoute exact path="/video/:id" component={Video} />
       <MyRoute path="*" component={Error} />
     </Switch>
