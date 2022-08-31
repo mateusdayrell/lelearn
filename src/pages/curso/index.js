fa8e2f61c06fd74cf781a5d6baf45c68c9e6d7d7
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-31 17:55:00 -0300

    feat: add curso page

--- src/pages/curso/index.js
+++ src/pages/curso/index.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 import './style.css';
 import Navbar from '../../components/Navbar';
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
 
-export default function Curso() {
+export default function Cursos() {
+  const history = useHistory();
+  const params = useParams();
+
+  const [curso, setCurso] = useState({});
+  const [videos, setVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getCurso();
+  }, []);
+
+  const getCurso = async () => {
+    const { cod_curso } = params;
+    setIsLoading(true);
+
+    try {
+      const { data } = await axios.get(`/cursos/${cod_curso}`);
+
+      setCurso(data);
+      setVideos(data.videos);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  const handleRedirect = (cod_video) => {
+    history.push(`/videos/${cod_video}`);
+  };
+
   return (
     <>
       <Navbar />
+      <Loading isLoading={isLoading} />
       <h1>Cursos</h1>
-      <button type="button">Botão</button>
+      <div className="container-video">
+        <div>{curso.nome_curso}</div>
+        <h3>Videos</h3>
+        {videos.map((video) => (
+          <div key={video.cod_video}>
+            <div
+              className="video"
+              onClick={() => handleRedirect(video.cod_video)}
+            >
+              {video.titulo_video}
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
