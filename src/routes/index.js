1cfed12467999847c819b03e5d2f58f654cf1b34
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-31 17:57:18 -0300

    feat: add curso and cursos

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import Video from '../pages/Video';
+import Cursos from '../pages/Cursos';
 import Curso from '../pages/Curso';
 import Treinamento from '../pages/Treinamento';
 import Error from '../pages/Error';
@@ -18,9 +19,10 @@ export default function Routes() {
       <MyRoute exact path="/" component={Home} />
       <MyRoute exact path="/login" component={Login} />
       <MyRoute exact path="/treinamentos" component={Treinamento} isClosed />
-      <MyRoute exact path="/curso" component={Curso} isClosed />
+      <MyRoute exact path="/cursos/:cod_curso" component={Curso} isClosed />
+      <MyRoute exact path="/cursos" component={Cursos} isClosed />
       <MyRoute exact path="/usuarios" component={AdmUsuario} isClosed />
-      <MyRoute exact path="/video/:id" component={Video} />
+      <MyRoute exact path="/videos/:cod_video" component={Video} isClosed />
       <MyRoute path="*" component={Error} />
     </Switch>
   );
