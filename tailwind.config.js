40aabb84a4acbeee6474ceafc6e4146cb34d92d2
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-09-16 18:30:11 -0300

    feat: add colors on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.js'
-  ],
+  content: ['./src/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
-        sans: 'Montserrat, sans-serif'
+        sans: 'Montserrat, sans-serif',
       },
       colors: {
-        verde:{
-          100: '#1fd660',
-          200: '#18a84b',
+        verde: {
+          100: '#00B37E',
+          200: '#015F43',
           300: '#0e662d',
-        }
-      }
+          600: 'rgba(0, 179, 126, 0.6)',
+        },
+        cinza: {
+          50: '#EAF0F7',
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          400: '#8D8D99',
+          600: '#323238',
+          700: '#1e1e1e',
+          800: '#121214',
+        },
+        azul: {
+          100: '#81D8F7',
+        },
+        laranja: {
+          100: '#FBA94C',
+        },
+        vermelho: {
+          100: '#F75A68',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
