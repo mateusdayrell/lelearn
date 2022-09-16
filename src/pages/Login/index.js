0083ca59ba53ca4b5e326d9f86ba0cceec4384e1
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-09-16 18:31:11 -0300

    refactor: style login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -39,32 +39,34 @@ export default function Login(props) {
   return (
     <>
       <Loading isLoading={isLoading} />
+      <div className="elipse-1" />
+      <div className="green-gradient h-full w-full flex justify-end float">
+        <div className="flex flex-col h-min w-min mt-[30vh] mr-[5vw]">
+          <h1 className="mx-auto font-extrabold text-2xl text-white">Login</h1>
 
-      <div className="login-container">
-        <h1 className="mx-auto font-extrabold text-2xl">Login</h1>
-
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="email"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            name="senha"
-            placeholder="senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-          />
-          <button className="btn" type="submit">
+          <div className="flex flex-col gap-3 w-[40vw] max-w-[350px]">
+            <input
+              type="text"
+              name="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              name="senha"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+          </div>
+          <button className="btn mt-3" type="button" onClick={handleSubmit}>
             Logar
           </button>
-        </form>
-        <Link className="mx-auto text-blue-600" to="/recuperar-senha">
-          Recuperar senha
-        </Link>
+          <Link className="mx-auto text-blue-600" to="/recuperar-senha">
+            Recuperar senha
+          </Link>
+        </div>
       </div>
     </>
   );
