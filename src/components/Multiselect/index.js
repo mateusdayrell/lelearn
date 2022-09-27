5e8566fd65ee0119517c56bb1ebaec303dabde66
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-09-27 18:00:37 -0300

    feat: add Multiselect on components

---
+++ src/components/Multiselect/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AiOutlineClose } from "react-icons/ai";
+
+export default function Multiselect({ type, array, treinamento, value, label, handleChange, handleRemove }) {
+  const verifyEqual = (cod) => {
+    let controle = false
+
+    treinamento.forEach(element => {
+      if(element[value] === cod) controle = true
+    });
+
+    return controle
+  }
+
+  const handleSelectChange = (eventValue) => {
+    handleChange(type, eventValue)
+    document.getElementById(`select-${type}`).selectedIndex = 0 // resetar valor do select apos selecionar
+  }
+
+  return (
+      <div className="my-2 p-1 flex flex-col gap bg-[#323238] rounded svelte-1l8159u">
+        <div className="flex flex-auto flex-wrap">
+          {treinamento.map((trein) => (
+                <div
+                  key={trein[value]}
+                  className="flex justify-center items-center m-1 py-1 px-2 rounded-full text-gray-100 bg-verde-100 border border-gray-400 gap-1"
+                >
+                  <div className="text-xs leading-none max-w-full flex-initial">
+                    {trein[label]}
+                  </div>
+                  <div className="flex flex-auto">
+                      <button type='button' onClick={() => handleRemove(type, trein[value])}>
+                          <AiOutlineClose/>
+                      </button>
+                  </div>
+                </div>
+              ))
+            }
+        </div>
+
+        <div className="flex-1 border-b border-gray-300 pt-2">
+          <select
+            name={type}
+            id={`select-${type}`}
+            onChange={e => handleSelectChange(e.target.value)}>
+              <option value="" disabled selected>Selecione um {type === 'usuarios' ? 'usuário' : 'curso'}</option>
+              {array.length > 0
+                ? array.map((el) => (
+                    <option
+                      key={el[value]}
+                      value={JSON.stringify(el)}
+                      className={verifyEqual(el[value]) ? "bg-verde-100 text-gray-50" : ''} disabled={verifyEqual(el.cpf)}
+                    >
+                      {el[label]}
+                    </option>
+                  ))
+                : ''}
+          </select>
+        </div>
+      </div>
+  );
+}
+
+Multiselect.defaultProps = {
+  type: 'usuarios',
+  array: [],
+  treinamento: [],
+  value: 'cpf',
+  label: 'nome',
+  handleChange: () => null,
+  handleRemove: () => null,
+};
+
+Multiselect.propTypes = {
+  type: PropTypes.string,
+  array: PropTypes.array,
+  treinamento: PropTypes.array,
+  value: PropTypes.string,
+  label: PropTypes.string,
+  handleChange: PropTypes.func,
+  handleRemove: PropTypes.func,
+};
