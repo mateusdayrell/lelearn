c4e0eacb414f592958f27e8ce533553bff447a0a
Matheus Sousa <matheussousab2014@gmail.com>
2022-09-30 11:05:50 -0300

    alterações icones e navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import {
-  FaHome,
-  FaVideo,
-  FaUser,
-  FaPowerOff,
-  FaClone,
-  FaGripHorizontal,
-} from 'react-icons/fa';
+import { BookBookmark, Users, SignOut, BookOpen, YoutubeLogo, MonitorPlay } from "phosphor-react";
 
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './styles.css';
 import * as actions from '../../store/modules/auth/actions';
 import history from '../../services/history';
-import logoLigth from '../../assets/lelearn-logo-ligth.png';
 
 export default function Navbar() {
   const dispatch = useDispatch();
@@ -38,7 +30,7 @@ export default function Navbar() {
     <div className="navbar">
       <div className='logo-content'>
         <Link className="link" to="/">
-          <svg width="30" height="27" viewBox="0 0 39 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="27" height="27" viewBox="0 0 39 36" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6.36963 31.2345H19.5V36H0V0H6.36963V31.2345Z" fill="#00B37E" />
             <path d="M25.8696 31.2345H39V36H19.5V0H25.8696V31.2345Z" fill="#00B37E" />
           </svg>
@@ -48,34 +40,34 @@ export default function Navbar() {
         <ul>
           <li>
             <Link className="link" to="/videos">
-              <FaVideo className="icon" size={24} />
+              <YoutubeLogo size={24} />
             </Link>
           </li>
           <li>
             <Link className="link" to="/cursos">
-              <FaClone className="icon" size={24} />
+              <BookBookmark size={24} />
             </Link>
           </li>
           <li>
             <Link className="link" to="/treinamentos">
-              <FaGripHorizontal className="icon" size={24} />
+              <BookOpen size={24} />
             </Link>
           </li>
           <li>
             <Link className="link" to="/usuarios">
-              <FaUser className="icon" size={24} />
+              <Users size={24} />
             </Link>
           </li>
           <li>
             <Link className="link" to="/gestao-videos">
-              <FaVideo className="icon" size={24} />
+              <MonitorPlay size={24} />
             </Link>
           </li>
         </ul>
       </div>
       <div className='profile-content'>
         <Link onClick={handleLogout} className="link" to="/login">
-          <FaPowerOff className="icon" size={24} />
+          <SignOut size={24} />
         </Link>
       </div>
     </div >
