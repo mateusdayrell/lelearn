b79b890883d25e9e2b0ce1dfc43a47b451dc25cc
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-10-14 17:07:47 -0300

    refactor: corrigir rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,25 +20,37 @@ import GestaoTreinamentos from '../pages/Admin/GestaoTreinamentos';
 export default function Routes() {
   const tipoUsuario = useSelector((state) => state.auth.usuario.tipo); // TIPO -> 0 === ADMINISTRADOR, TIPO -> 1 === USUARIO COMUM
   return (
-    <Switch>
-      <MyRoute exact path="/" component={Home} />
-      <MyRoute exact path="/login" component={Login} />
-      <MyRoute exat path="/recuperar-senha" component={RecuperarSenha} />
-      <MyRoute exact path="/treinamentos" component={Treinamento} isClosed />
-      <MyRoute exact path="/cursos/:cod_curso" component={Curso} isClosed />
-      <MyRoute exact path="/cursos" component={Cursos} isClosed />
-      <MyRoute exact path="/videos/:cod_curso/:cod_video" component={Video} isClosed />
-      <MyRoute path="*" component={Error} />
-
-      {tipoUsuario === 0 ? // ADMINISTRADOR
-        <>
+    <>
+      {tipoUsuario === 0 && // ADMINISTRADOR
+        <Switch>
           <MyRoute exact path="/usuarios" component={Usuario} isClosed />
           <MyRoute exact path="/gestao-videos" component={GestaoVideos} isClosed />
           <MyRoute exact path="/gestao-cursos" component={GestaoCursos} isClosed />
           <MyRoute exact path="/gestao-treinamentos" component={GestaoTreinamentos} isClosed />
-        </>
-      : ''}
 
-    </Switch>
+          <MyRoute exact path="/" component={Home} />
+          <MyRoute exact path="/login" component={Login} />
+          <MyRoute exat path="/recuperar-senha" component={RecuperarSenha} />
+          <MyRoute exact path="/treinamentos" component={Treinamento} isClosed />
+          <MyRoute exact path="/cursos/:cod_curso" component={Curso} isClosed />
+          <MyRoute exact path="/cursos" component={Cursos} isClosed />
+          <MyRoute exact path="/videos/:cod_curso/:cod_video" component={Video} isClosed />
+          <MyRoute path="*" component={Error} />
+        </Switch>
+      }
+
+      {tipoUsuario === 1 && // USUARIO COMUM
+        <Switch>
+          <MyRoute exact path="/" component={Home} />
+          <MyRoute exact path="/login" component={Login} />
+          <MyRoute exat path="/recuperar-senha" component={RecuperarSenha} />
+          <MyRoute exact path="/treinamentos" component={Treinamento} isClosed />
+          <MyRoute exact path="/cursos/:cod_curso" component={Curso} isClosed />
+          <MyRoute exact path="/cursos" component={Cursos} isClosed />
+          <MyRoute exact path="/videos/:cod_curso/:cod_video" component={Video} isClosed />
+          <MyRoute path="*" component={Error} />
+        </Switch>
+      }
+    </>
   );
 }
