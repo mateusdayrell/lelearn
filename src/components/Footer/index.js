e0b39483577322399e694b4892eff0d6b45eba2b
Matheus Sousa <matheussousab2014@gmail.com>
2022-10-18 15:39:13 -0300

    feat: changes navbar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="text-center bg-transparent z-0 opacity-80 h-12 pt-4 mt-4 pb-10 border-t w-full ">
+    <footer className="text-center bg-transparent z-0 opacity-80 h-12 pt-4 mt-4 pb-10 border-t w-full  border-cinza-300">
       <p className='text-gray-50 text-sm'>
         <span className='font-semibold'>Copyright © 2022 - NewCoders. </span>
         Todos os direitos reservados
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -21,22 +21,22 @@ import history from '../../services/history';
 export default function Navbar() {
   const dispatch = useDispatch();
 
-  const {nome, tipo} = useSelector((state) => state.auth.usuario); // TIPO -> 0 === ADMINISTRADOR, TIPO -> 1 === USUARIO COMUM
+  const { nome, tipo } = useSelector((state) => state.auth.usuario); // TIPO -> 0 === ADMINISTRADOR, TIPO -> 1 === USUARIO COMUM
 
-  const selected = "w-8 h-8 bg-verde-100 hover:bg-verde-200 flex items-center justify-center rounded-md text-cinza-500"
+  const selected = "w-8 h-8 bg-cinza-100 hover:bg-verde-200 flex items-center justify-center rounded-md text-cinza-500"
   const [nomeUsuario, setNomeUsuario] = useState('Não logado');
   const [open, setOpen] = useState(true);
   const [word1, setWord1] = useState('')
   const [word2, setWord2] = useState('')
 
   const menus = [
-    { name: "Página Inicial", link: "/home", icon: House, users: [0, 1]},
+    { name: "Página Inicial", link: "/home", icon: House, users: [0, 1] },
     { name: "Videos", link: "/videos", icon: YoutubeLogo, users: [0, 1] },
-    { name: "Cursos", link: "/cursos", icon: BookBookmark, users:  [0, 1]},
+    { name: "Cursos", link: "/cursos", icon: BookBookmark, users: [0, 1] },
     { name: "Treinamentos", link: "/treinamentos", icon: Bookmark, users: [0, 1] },
     { name: "Gestão de Usuários", link: "/usuarios", icon: Users, users: [0], margin: true },
     { name: "Gestão de Vídeos", link: "/gestao-videos", icon: MonitorPlay, users: [0] },
-    { name: "Gestão de Cursos", link: "/gestao-cursos", icon: Books, users: [0]},
+    { name: "Gestão de Cursos", link: "/gestao-cursos", icon: Books, users: [0] },
     { name: "Gestão de Treinamentos", link: "/gestao-treinamentos", icon: BookmarksSimple, users: [0] },
   ];
 
@@ -52,17 +52,17 @@ export default function Navbar() {
   const handleWords = (name) => {
     const nomes = name.toUpperCase().split(' ')
 
-    if(nomes.length === 1) {
-      setWord1(nomes[0].slice(0,1))
-      setWord2(nomes[0].slice(1,2))
+    if (nomes.length === 1) {
+      setWord1(nomes[0].slice(0, 1))
+      setWord2(nomes[0].slice(1, 2))
     } else {
-      setWord1(nomes[0].slice(0,1))
-      setWord2(nomes.pop().slice(0,1))
+      setWord1(nomes[0].slice(0, 1))
+      setWord2(nomes.pop().slice(0, 1))
     }
   }
 
   const handleCurrentMenu = (link) => {
-    if(window.location.href.includes(link)) return true
+    if (window.location.href.includes(link)) return true
     return false
   }
 
@@ -85,8 +85,8 @@ export default function Navbar() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M6.36963 31.2345H19.5V36H0V0H6.36963V31.2345Z"/>
-            <path d="M25.8696 31.2345H39V36H19.5V0H25.8696V31.2345Z"/>
+            <path d="M6.36963 31.2345H19.5V36H0V0H6.36963V31.2345Z" />
+            <path d="M25.8696 31.2345H39V36H19.5V0H25.8696V31.2345Z" />
           </svg>
         </Link>
         <CaretDoubleRight
@@ -103,27 +103,31 @@ export default function Navbar() {
           menu.users.includes(tipo) ? // Verificar tipo do usuário
             <Link to={menu?.link} key={menu.link} className="group">
               <div
-                className={ handleCurrentMenu(menu.link) ? `${selected}` : "icons"}> {React.createElement(menu?.icon, { size: "24" })} </div>
+                className={handleCurrentMenu(menu.link) ? `${selected}` : "icons"}> {React.createElement(menu?.icon, { size: "24" })} </div>
               <span
-                style={{transitionDelay: `${i + 4}0ms`,}}
-                className={`whitespace-pre duration-500 ${open && "hidden"}`}
+                style={{ transitionDelay: `${i + 4}0ms`, }}
+                className={`whitespace-pre duration-500 ${open && "hidden"} `}
               >
                 {menu?.name}
               </span>
               <span className={`${!open && 'hidden'} absolute left-48 bg-verde-100 font-semibold whitespace-pre text-cinza-500 w-0 overflow-hidden
                 rounded-md drop-shadow-lg px-0 py-0 group-hover:px-2 group-hover:h-8 group-hover:left-14 group-hover:duration-300 group-hover:w-fit
                 flex items-center`}>
-                  {menu?.name}
+                {menu?.name}
               </span>
             </Link>
-          : ''
-      ))}
+            : ''
+        ))}
       </div>
 
       {/* USUARIO */}
       <div className={` ${open ? "profile-content-closed" : "profile-content-open"} profile-content`}>
-        <span className='avatar-name'>{word1}{word2}</span>
-        <span className={`${open && "hidden"} name`}>{nomeUsuario}</span>
+        <span className='avatar-name'>
+          {word1}{word2}
+        </span>
+        <span className={`${open && "hidden"} name`}>
+          {nomeUsuario}
+        </span>
         <Link onClick={handleLogout} className={`${open && "hidden"} link`} to="/login">
           <SignOut size={24} />
           Sair
