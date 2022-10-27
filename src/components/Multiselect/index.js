bf085e84008c862cc0a623d2a09ee8baf0201c02
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-10-27 18:57:43 -0300

    refactor: refatorar Multiselect

--- src/components/Multiselect/index.js
+++ src/components/Multiselect/index.js
@@ -2,35 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { X } from 'phosphor-react';
 
-export default function Multiselect({ type, arrLista, arrSuperior, value, label, handleMultiSelectChange, handleMultiSelectRemove }) {
+import { formatUserObj } from '../../helpers/formatUserObj'
+
+export default function Multiselect({ type, listaArr, array, setArray, value, label }) {
   const verifyEqual = (cod) => {
     let controle = false
 
-    arrSuperior.forEach(element => {
+    array.forEach(element => {
       if(element[value] === cod) controle = true
     });
 
     return controle
   }
 
-  const handleIsChanging = (eventValue) => {
-    handleMultiSelectChange(type, eventValue)
+  const handleOrder = (arr) => {
+    arr.sort((a, b) => a[label] > b[label] ? 1 : -1) // ordenar por nome
+  }
+
+  const handleAdd = (valueSelect) => {
+    const tempArr = [...array]
+    const item = JSON.parse(valueSelect)
+
+    if (type === 'usuário') tempArr.push(formatUserObj(item))
+    else tempArr.push(item)
+
+    handleOrder(tempArr)
+
+    setArray(tempArr)
     document.getElementById(`select-${type}`).selectedIndex = 0 // resetar valor do select apos selecionar
+  }
+
+  const handleRemove = (index) => {
+    const tempArr = [...array]
+    tempArr.splice(index, 1)
+    setArray(tempArr)
   }
 
   return (
       <div className="my-2 p-1 flex flex-col gap bg-[#323238] rounded svelte-1l8159u">
         <div className="flex max-w-2xl overflow-scroll max-h-24 flex-wrap">
-          {arrSuperior.map((trein) => (
+          {array.map((item, index) => (
                 <div
-                  key={`sup${trein[value]}${type}`}
+                  key={`sup${item[value]}${type}`}
                   className="flex flex-none justify-center items-center m-1 py-1.5 px-2.5 rounded-full text-gray-50 bg-verde-100 hover:bg-verde-200 border border-gray-400 hover:border-verde-200 gap-1 flex-wrap"
                 >
                   <div className="text-xs leading-none max-w-full flex-initial">
-                    {trein[label]}
+                    {item[label]}
                   </div>
                   <div className="flex flex-auto">
-                      <button type='button' className='hover:text-red-500' title={`Remover ${type}`} onClick={() => handleMultiSelectRemove(type, trein[value])}>
+                      <button type='button' className='hover:text-red-500' title={`Remover ${type}`} onClick={() => handleRemove(index)}>
                         <X weight="bold" />
                       </button>
                   </div>
@@ -44,10 +64,10 @@ export default function Multiselect({ type, arrLista, arrSuperior, value, label,
             name={type}
             id={`select-${type}`}
             defaultValue=""
-            onChange={e => handleIsChanging(e.target.value)}>
+            onChange={e => handleAdd(e.target.value)}>
               <option value="" disabled>Selecione um {type}</option>
-              {arrLista.length > 0
-                ? arrLista.map((el) => (
+              {listaArr.length > 0
+                ? listaArr.map((el) => (
                     <option
                       key={`inf${el[value]}${type}`}
                       value={JSON.stringify(el)}
@@ -65,20 +85,18 @@ export default function Multiselect({ type, arrLista, arrSuperior, value, label,
 
 Multiselect.defaultProps = {
   type: 'usuarios',
-  arrLista: [],
-  arrSuperior: [],
+  listaArr: [],
+  array: [],
+  setArray: () => null,
   value: 'cpf',
   label: 'nome',
-  handleMultiSelectChange: () => null,
-  handleMultiSelectRemove: () => null,
 };
 
 Multiselect.propTypes = {
   type: PropTypes.string,
-  arrLista: PropTypes.array,
-  arrSuperior: PropTypes.array,
+  listaArr: PropTypes.array,
+  array: PropTypes.array,
+  setArray: PropTypes.func,
   value: PropTypes.string,
   label: PropTypes.string,
-  handleMultiSelectChange: PropTypes.func,
-  handleMultiSelectRemove: PropTypes.func,
 };
