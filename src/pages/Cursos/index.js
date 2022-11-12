7ea8394db3f2f16759cee008ef2c1c661cbad9a1
mateusdayrell <mateusdayrell7@gmail.com>
2022-11-11 21:20:32 -0300

    feat: corrigir imagem em Cursos

--- src/pages/Cursos/index.js
+++ src/pages/Cursos/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { get } from 'lodash';
 import { FaFileImage } from 'react-icons/fa';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import './style.css';
 import Loading from '../../components/Loading';
@@ -10,7 +10,6 @@ import axios from '../../services/axios';
 
 
 export default function Cursos() {
-  const dispatch = useDispatch();
   const { cpf } = useSelector((state) => state.auth.usuario);
   const [cursos, setCursos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -40,11 +39,11 @@ export default function Cursos() {
           <div className='flex justify-between items-center border  text-white w-full  mb-3 pr-3 '>
           <div key={curso.cod_curso} className="flex items-center gap-3  ">
               {get(curso, 'nome_arquivo', false) ?
-                <img className='h-[110px] w-[150px]' src={curso.nome_arquivo} alt="Imagem do curso" />
+                <img className='h-[110px] w-[150px]' src={`${process.env.REACT_APP_BACKEND_URL}/images/${curso.nome_arquivo}`} alt="Imagem do curso" />
                 : <FaFileImage size={36}/>
               }
               <div className='Cursos-info'>
-              <h2 className=''>{curso.nome_curso}</h2>
+              <h2 className=''>{`${process.env.REACT_APP_BACKEND_URL}/${curso.nome_arquivo}`}</h2>
               <p className='text-xs'>{curso.desc_curso}</p>
               </div>
               </div>
