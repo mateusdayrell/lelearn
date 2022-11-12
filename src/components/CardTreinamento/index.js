9e799110a265b464be46164d92348518c7f6d844
Matheus Sousa <matheussousab2014@gmail.com>
2022-11-11 23:41:53 -0300

    feat: etilização do card de treinamento

--- src/components/CardTreinamento/index.js
+++ src/components/CardTreinamento/index.js
@@ -6,12 +6,14 @@ import './styles.css';
 export default function CardTreinamento({ treinamento }) {
     return (
         <div className='CardContainer'>
-            <div className='TopCard'>
-                <span className='text-md'>{treinamento.nome_treinamento}</span>
-            </div>
-            <div className='h-full w-full p-2'>
+            <div className='TopCard'></div>
+            <div className='h-[75%] p-2 flex flex-col justify-between'>
+                <div className='flex flex-col'>
+                    <span className='text-xs text-cinza-200'>Prazo do Usuário</span>
+                    <span className='NomeTreinamento'>{treinamento.nome_treinamento}</span>
+                </div>
                 <div className='w-full flex items-center justify-center'>
-                <button className='BtnCardTreinamento'><Lightning size={20} />Acessar</button>
+                    <button className='BtnCardTreinamento'><Lightning size={20} />Acessar</button>
                 </div>
             </div>
         </div>
