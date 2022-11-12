22c87638e17c19319c8c36c1d21cde91afae69a7
Matheus Sousa <matheussousab2014@gmail.com>
2022-11-12 14:17:35 -0300

    feat: estilização card treinamento

--- src/components/CardTreinamento/index.js
+++ src/components/CardTreinamento/index.js
@@ -7,14 +7,15 @@ export default function CardTreinamento({ treinamento }) {
     return (
         <div className='CardContainer'>
             <div className='TopCard'></div>
-            <div className='h-[75%] p-2 flex flex-col justify-between'>
+            <div className='h-[75%] p-3 flex flex-col'>
                 <div className='flex flex-col'>
                     <span className='text-xs text-cinza-200'>Prazo do Usuário</span>
                     <span className='NomeTreinamento'>{treinamento.nome_treinamento}</span>
+                    <small>{treinamento.desc_treinamento}</small>
                 </div>
-                <div className='w-full flex items-center justify-center'>
-                    <button className='BtnCardTreinamento'><Lightning size={20} />Acessar</button>
-                </div>
+            </div>
+            <div className='w-full flex items-center justify-center'>
+                <button className='BtnCardTreinamento'><Lightning size={20} />Acessar</button>
             </div>
         </div>
     );
