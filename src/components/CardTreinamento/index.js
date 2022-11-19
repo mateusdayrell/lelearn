ff2f6c6bf3ec1ef93e13a93720757d1c8dc49097
Matheus Sousa <matheussousab2014@gmail.com>
2022-11-19 17:46:58 -0300

    feat: adicionado porcentagem card treinamento

--- src/components/CardTreinamento/index.js
+++ src/components/CardTreinamento/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { CaretRight } from 'phosphor-react';
 import moment from 'moment/moment';
@@ -8,27 +8,38 @@ import './styles.css';
 
 export default function CardTreinamento({ treinamento }) {
   const history = useHistory();
+  const [percentage, setPercentage] = useState('');
+
+  useEffect(() => {
+    handlePercentage(treinamento.cursos_assistidos,treinamento.total_cursos);
+  }, []);
 
   const handleRedirect = (cod_treinamento) => {
     history.push(`/treinamentos/${cod_treinamento}`);
   };
 
-  const handlePercentage = (watched,total) => {
-    const percentage = watched/total * 100;
-    return percentage
+  const handlePercentage = (watched, total) => {
+    const percent = total == 0 ? 0 : Math.floor(watched / total * 100);
+    // if (percent === Infinity) return setPercentage(0);
+    return setPercentage(percent)
   }
+
   return (
     <div className='CardContainer'>
       <div className='ContainerLeftCard'>
+        <div className='w-full'>
+          <div style={{width: `${percentage}%`}} className="bg-vermelho-100">{percentage}</div>
+        </div>
         <div className='h-[80%] px-3 py-1 flex flex-col'>
           <div className='flex flex-col'>
+            <small>{treinamento.cursos_assistidos}</small>
+            <small>{treinamento.total_cursos}</small>
             <small>
               {treinamento.prazo === null
                 ? <p className='PrazoListAzul'>Prazo não determinado</p>
                 : <p className='PrazoListAmarelo'>{moment(treinamento.prazo).format('LL')}</p>
               }
             </small>
-            <small>{handlePercentage(treinamento.cursos_assistidos,treinamento.total_cursos) === 100 ? 'Concluido':'Em andamento'}</small>
             <span className='NomeTreinamento'>{treinamento.nome_treinamento}</span>
             {/* <small className='DescTreinamento'>{treinamento.desc_treinamento}</small> */}
           </div>
