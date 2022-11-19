9c840b11aa9af1b75cb3347731ab632d4a34ceac
Matheus Sousa <matheussousab2014@gmail.com>
2022-11-18 22:27:27 -0300

    feat: adição animações card treinamento

--- src/components/CardTreinamento/index.js
+++ src/components/CardTreinamento/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Lightning } from 'phosphor-react';
+import { CaretRight } from 'phosphor-react';
 import moment from 'moment/moment';
 import { useHistory } from 'react-router-dom';
 
@@ -13,39 +13,38 @@ export default function CardTreinamento({ treinamento }) {
     history.push(`/treinamentos/${cod_treinamento}`);
   };
 
-    return (
-        <div className='CardContainer'>
-            <div className='ContainerLeftCard'>
-                <div className='h-[75%] px-3 py-1 flex flex-col'>
-                    <div className='flex flex-col'>
-                        <small>
-                          {treinamento.prazo === null
-                            ? <p className='PrazoListAzul'>Prazo não determinado</p>
-                            : <p className='PrazoListAmarelo'>{moment(treinamento.prazo).format('DD-MM-YYYY')}</p>
-                          }
-                        </small>
-                        <span className='NomeTreinamento'>{treinamento.nome_treinamento}</span>
-                        <small className='DescTreinamento'>{treinamento.desc_treinamento}</small>
-                    </div>
-                </div>
-                <div className='w-full pl-3 text-black'>
-                    <button
-                      type='button'
-                      className='BtnCardTreinamento'
-                      onClick={() => handleRedirect(treinamento.cod_treinamento)}>
-                        <Lightning size={20} />Acessar
-                      </button>
-                </div>
-            </div>
-            <div className='ContainerRightCard'/>
+  return (
+    <div className='CardContainer'>
+      <div className='ContainerLeftCard'>
+        <div className='h-[80%] px-3 py-1 flex flex-col'>
+          <div className='flex flex-col'>
+            <small>
+              {treinamento.prazo === null
+                ? <p className='PrazoListAzul'>Prazo não determinado</p>
+                : <p className='PrazoListAmarelo'>{moment(treinamento.prazo).format('LL')}</p>
+              }
+            </small>
+            <span className='NomeTreinamento'>{treinamento.nome_treinamento}</span>
+            <small className='DescTreinamento'>{treinamento.desc_treinamento}</small>
+          </div>
         </div>
-    );
+        <div className='w-full flex justify-end text-black px-1'>
+          <button
+            type='button'
+            className='BtnCardTreinamento'
+            onClick={() => handleRedirect(treinamento.cod_treinamento)}>
+            <span><CaretRight size={20} /></span>Acessar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 CardTreinamento.defaultProps = {
-    treinamento: {},
+  treinamento: {},
 };
 
 CardTreinamento.propTypes = {
-    treinamento: PropTypes.object,
+  treinamento: PropTypes.object,
 };
