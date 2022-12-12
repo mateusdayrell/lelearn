3a7e6d17cb2158992ab2a49873ea96bb615e6ac3
Matheus Sousa <matheussousab2014@gmail.com>
2022-12-12 19:09:19 -0300

    feat: adicionar calendario

---
+++ src/components/Calendario/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+import './Calendar.css';
+
+export default function Calendario() {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <Calendar onChange={onChange} value={value} calendarType='Hebrew' showNavigation={false}/>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Calendario from '../../components/Calendario';
+import moment from 'moment/moment';
 
 import './style.css';
 
 export default function Home() {
   const location = useLocation();
 
-  const { nome, tipo } = useSelector((state) => state.auth.usuario);
+  const { nome } = useSelector((state) => state.auth.usuario);
 
   const [nomeUsuario, setNomeUsuario] = useState('Não logado');
 
@@ -22,6 +24,20 @@ export default function Home() {
     <>
       <div className='container-body'>
         <h1 className='title'>Olá, {nomeUsuario}!</h1>
+
+        <div className='flex gap-2'>
+          <div className='w-3/4 h-full text-cinza-200'>
+            Teste
+          </div>
+
+          <div className='ContainerLateralHome'>
+            <h2 className='text-laranja-100 font-semibold'>Cronograma</h2>
+            <p className='text-xs text-cinza-600 font-semibold capitalize'>{moment().format('dddd')}, {moment().format('LL')}</p>
+            <div className='mt-2'>
+              <Calendario />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
