e1571cfe5b2c5893c5371e4f5ed6a2419e87289b
mateusdayrell <mateusdayrell7@gmail.com>
2022-12-17 18:05:41 -0300

    refactor: remover espaço da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -68,7 +68,7 @@ export default function Home() {
                   key={treinamento.cod_treinamento}
                   treinamento={treinamento} />
               ))}
-              
+
             </div>
           </div>
         </div>
