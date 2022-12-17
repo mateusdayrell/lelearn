edbbe1b8b43a2306a0c4e5024d1fb6939f008b9b
mateusdayrell <mateusdayrell7@gmail.com>
2022-12-17 12:46:05 -0300

    refactor: modificar baseUrl do axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://llrn-backend-sjy1-ki9guwrt4-mateusdayrell.vercel.app', // url da aplicação
+  baseURL: 'https://llrn-backend-sjy1.vercel.app', // url da aplicação
 
 });
