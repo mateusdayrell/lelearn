9489d95cd4ab820730f9a9580a16b2c300d8e079
Matheus Sousa <matheussousab2014@gmail.com>
2022-12-20 16:13:40 -0300

    refactor: mensagem verificação nenhum treinamento

--- src/pages/Treinamentos/index.js
+++ src/pages/Treinamentos/index.js
@@ -11,48 +11,50 @@ import history from '../../services/history';
 // import './style.css';
 
 export default function Treinamentos() {
-    const dispatch = useDispatch();
-    const { cpf } = useSelector((state) => state.auth.usuario);
-    const [treinamentos, setTreinamentos] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+  const { cpf } = useSelector((state) => state.auth.usuario);
+  const [treinamentos, setTreinamentos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        getTreinamentos();
-    }, []);
+  useEffect(() => {
+    getTreinamentos();
+  }, []);
 
-    const getTreinamentos = async () => {
-      try {
-        setIsLoading(true);
-        const { data } = await axios.get(`/usuarios/get-treinamentos/${cpf}`);
-        setTreinamentos(data);
-        setIsLoading(false);
-      } catch (error) {
-        setIsLoading(false);
-        const { erros } = error.response.data;
-        erros.map((err) => toast.error(err));
+  const getTreinamentos = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(`/usuarios/get-treinamentos/${cpf}`);
+      setTreinamentos(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      const { erros } = error.response.data;
+      erros.map((err) => toast.error(err));
 
-        if(error.response.status === 401) {
-          dispatch(loginFailure());
-          history.push('/login');
-        }
+      if (error.response.status === 401) {
+        dispatch(loginFailure());
+        history.push('/login');
       }
-    };
+    }
+  };
 
-    return (
-        <>
-            <Loading isLoading={isLoading} />
-            <div className='container-body'>
-                <h1 className='title'>Treinamentos</h1>
+  return (
+    <>
+      <Loading isLoading={isLoading} />
+      <div className='container-body'>
+        <h1 className='title'>Treinamentos</h1>
 
-                <div className='flex gap-3 flex-wrap'>
-                    {treinamentos.map((treinamento) => (
-                        <CardTreinamento
-                            key={treinamento.cod_treinamento}
-                            treinamento={treinamento} />
-                    ))}
-                </div>
+        <div className='flex gap-3 flex-wrap'>
+          {!treinamentos || treinamentos.length === 0
+            ? <p className='text-cinza-300 text-xs'>Você não possui treinamentos.</p>
+            : treinamentos.map((treinamento) => (
+              <CardTreinamento
+                key={treinamento.cod_treinamento}
+                treinamento={treinamento} />
+            ))}
+        </div>
 
-            </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
